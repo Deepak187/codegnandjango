193239714b2b52858263d0ce3ef4765d3545dd90
Deepak <deepakflushymax@gmail.com>
2020-09-08 14:40:40 +0530

    settings.py

--- codegnan/settings.py
+++ codegnan/settings.py
@@ -16,9 +16,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-#BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -81,7 +80,7 @@ WSGI_APPLICATION = 'codegnan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
@@ -121,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-    
+STATICFILES_DIRS = [BASE_DIR, 'assets', ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
