810d581ca9aa3c2361da584b23fe8d58ca63ecc2
Deepak <deepakflushymax@gmail.com>
2020-09-08 15:17:32 +0530

    missiongoing

--- codegnan/settings.py
+++ codegnan/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR, 'assets', ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
