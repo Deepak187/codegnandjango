156b40c0cb3f5072d3223a3b19a4acafe4f47b76
Deepak <deepakflushymax@gmail.com>
2020-09-08 13:47:23 +0530

    staticrootremoved

--- codegnan/settings.py
+++ codegnan/settings.py
@@ -125,8 +125,6 @@ if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
